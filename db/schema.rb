93a1fb1e7335a744152b42044868d5c12c060feb
Rafael Ribeiro <rafael.ribeiro1705@gmail.com>
2025-07-05 02:01:11 -0300

    Criação de Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_04_141901) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_04_194838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
+
   create_table "carts", force: :cascade do |t|
-    t.decimal "total_price", precision: 17, scale: 2
+    t.decimal "total_price", precision: 17, scale: 2, default: "0.0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0, null: false
@@ -28,4 +38,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_04_141901) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
 end
