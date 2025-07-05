8f3b5d1b9932835114e28dc2d684b6fb77080ac5
Rafael Ribeiro <rafael.ribeiro1705@gmail.com>
2025-07-05 02:28:10 -0300

    Ajustando o Sidekiq

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,21 @@
 class MarkCartAsAbandonedJob
-  include Sidekiq::Job
+  include Sidekiq::Job # Job do Sidekiq
+  
+
+  queue_as :default    # Job colocado na fila "default"
 
   def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+
+    # Marca como abandonado 
+    Cart.where("updated_at < ? AND status = ?", 3.hours.ago, Cart.statuses[:active]).find_each do |cart| # Procura carrinhos inativos há mais de 3 horas
+      cart.update(status: :abandoned) # Atualiza Status para abandonado
+      Rails.logger.info "Carrinho ##{cart.id} marcado como abandonado" # Log para usuario
+    end
+
+    # Remove carrinhos abandonados
+    Cart.where("updated_at < ? AND status = ?", 7.days.ago, Cart.statuses[:abandoned]).find_each do |cart| # Procura carrinhos abandonados há mais de 7 dias
+      cart.destroy # Destroi Carrinho
+      Rails.logger.info "Carrinho ##{cart.id} removido por abandono prolongado" # Log para usuario
+    end
   end
 end
