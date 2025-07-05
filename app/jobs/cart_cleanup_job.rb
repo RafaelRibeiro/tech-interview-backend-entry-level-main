4af1173f33dc05ab211fca229a03d1d54aa6f41c
Rafael Ribeiro <rafael.ribeiro1705@gmail.com>
2025-07-05 02:28:30 -0300

    Job

---
+++ app/jobs/cart_cleanup_job.rb
@@ -0,0 +1,17 @@
+class CartCleanupJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    # Marca como abandonados os carrinhos sem interação há mais de 3 horas
+    Cart.where("updated_at < ? AND status != ?", 3.hours.ago, "abandoned").find_each do |cart|
+      cart.update(status: :abandoned)
+      Rails.logger.info "Carrinho ##{cart.id} marcado como abandonado."
+    end
+
+    # Remove carrinhos abandonados há mais de 7 dias
+    Cart.where("status = ? AND updated_at < ?", "abandoned", 7.days.ago).find_each do |cart|
+      cart.destroy
+      Rails.logger.info "Carrinho ##{cart.id} removido por abandono prolongado."
+    end
+  end
+end
