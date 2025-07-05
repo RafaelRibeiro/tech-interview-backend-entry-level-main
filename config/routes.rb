3f164f93cbd8f1e725d1297a2a511a71274dc4eb
Rafael Ribeiro <rafael.ribeiro1705@gmail.com>
2025-07-05 02:26:45 -0300

    Ajuste de Rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 require 'sidekiq/web'
+mount Sidekiq::Web => '/sidekiq'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Conjunto de rotas para o "Carrinho"
+  resource :cart, only: [:show, :create], controller: 'carts' do
+    post :add_item, on: :collection                                               # Altera a quantidade de um produto já existente no carrinho.
+    delete ':product_id', action: :remove_item, on: :collection, as: :remove_item # Remove um item do carrinho com base no ID do produto.
+  end
+
   root "rails/health#show"
 
   
-end
+  end
