6877cfc94a4a5725e424d19eb1b676cd5a0c1e2d
Rafael Ribeiro <rafael.ribeiro1705@gmail.com>
2025-07-05 02:28:50 -0300

    Ajuste de Cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,110 @@
 class CartsController < ApplicationController
   ## TODO Escreva a lógica dos carrinhos aqui
+  
+  # Executa Metodo load_or_create_cart antes de qualquer ação
+  before_action :load_or_create_cart
+
+  # Criação de Carrinho
+  def create
+    product = Product.find_by(id: params[:product_id]) # Busca o produto pelo ID pelos params
+    return render_not_found('Produto não encontrado') unless product # Retorna caso o produto não for encontrado com mensagem customizada
+
+    cart_item = @cart.add_product(product.id, params[:quantity].to_i) # Adiciona ou atualiza o item no carrinho
+
+    # Verifica se o item existe no BD
+    if cart_item.persisted?
+      @cart.touch(:updated_at) # Atualiza o updated_at para a hora atual.
+      @cart.update(status: :active) # Atualiza status do carrinho para ativo
+      render_cart # Chama metodo que Renderiza o carrinho.
+    # Senão
+    else
+      render_unprocessable_entity('Erro ao adicionar produto no carrinho') # Chama metodo que retorna um erro 422 com mensagem.
+    end
+  end
+
+  # Lista os produtos do carrinho
+  def show
+    render_cart # Chama metodo que renderiza o carrinho
+  end
+
+  # Altera a quantidade de um produto existente no carrinho
+  def add_item
+    product_id = params[:product_id]   # Captura o ID do produto enviado pelos params
+    quantity = params[:quantity].to_i  # Captura a qtd do produto enviado pelos params e converte para inteiro
+
+    cart_item = @cart.cart_items.find_by(product_id: product_id)   # Busca o item no carrinho pelo product_id
+    return render_not_found('Produto não está no carrinho') unless cart_item   # Se o item não existir no carrinho, retorna erro 404 com mensagem customizada.
+
+    # Se a quantidade for menor ou igual a 0
+    if quantity <= 0
+      return render_unprocessable_entity('Quantidade deve ser maior que zero') # Chama metodo que retorna um erro 422 com mensagem.
+    end
+
+    cart_item.update(quantity: quantity)   # Atualiza a quantidade do item no banco de dados
+    @cart.touch(:updated_at)   # Atualiza o updated_at para a hora atual.
+    @cart.update_total_price # Atualiza o preço total do carrinho
+    render_cart # Chama metodo que renderiza o carrinho
+  end
+
+  # Remove um item do carrinho
+  def remove_item
+    cart_item = @cart.cart_items.find_by(product_id: params[:product_id])   # Busca o item do carrinho pelo product_id enviado nos params
+    return render_not_found('Produto não encontrado no carrinho') unless cart_item # Se o item não existir no carrinho, retorna erro 404 com mensagem customizada.
+
+    cart_item.destroy # Destroi item do carrinho
+    @cart.touch(:updated_at) # Atualiza o updated_at para a hora atual.
+
+    # Se o carrinho de itens estiver vazio
+    if @cart.cart_items.empty?
+      @cart.update(status: :empty) # Atualiza status do carrinho para vazio
+    end
+
+    @cart.update_total_price # Atualiza o preço total do carrinho
+    render_cart # Chama metodo que renderiza o carrinho
+  end
+
+  # Metodos privados 
+  private
+
+  # Carrega o carrinho da sessão ou cria um novo
+  def load_or_create_cart
+    @cart = Cart.find_by(id: session[:cart_id]) # Carrega o carrinho usando o ID armazenado na sessão do usuário
+
+    # Se não encontrar o carrinho
+    unless @cart
+      @cart = Cart.create!  # Cria um novo carrinho 
+      session[:cart_id] = @cart.id # Salva o ID do novo carrinho na sessão para identificar o carrinho nas próximas requisições
+    end
+  end
+
+  # Renderiza o carrinho
+  def render_cart
+
+    # Renderiza a resposta JSON com os dados do carrinho e seus produtos
+    render json: {
+      id: @cart.id,  # ID do carrinho atual
+
+      # Lista de produtos no carrinho
+      products: @cart.cart_items.includes(:product).map do |item| 
+        {
+          id: item.product.id, # Id do produto
+          name: item.product.name, # Nome do produto
+          quantity: item.quantity, # Quantidade do produto
+          unit_price: item.product.price.to_f, # Preço unitário convertido para decimal
+          total_price: (item.quantity * item.product.price).to_f # preço total desse produto convertido para decimal
+        }
+      end,
+      total_price: @cart.total_price.to_f # Preço total de todos os produtos no carrinho convertido para decimal
+    }
+  end
+
+  # Retorna erro 404 com mensagem customizada no formato json
+  def render_not_found(message)
+    render json: { error: message }, status: :not_found
+  end
+
+  # Retorna erro 422 com mensagem customizada no formato json
+  def render_unprocessable_entity(message)
+    render json: { error: message }, status: :unprocessable_entity
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,56 @@
 class Cart < ApplicationRecord
-  validates_numericality_of :total_price, greater_than_or_equal_to: 0
+  has_many :cart_items, dependent: :destroy # Associação de um carrinho para varios itens
+  has_many :products, through: :cart_items  # Associação indireta: Um carrinho possui muitos produtos através dos itens no carrinho
+
+  validates_numericality_of :total_price, greater_than_or_equal_to: 0  # Validação e garante que o preço total do carrinho seja um número >= 0
+
+  enum status: { active: 0, abandoned: 1, empty: 2 } # Status do carrinho
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+
+  # Adiciona ou atualizar um produto no carrinho 
+  def add_product(product_id, quantity)
+    item = cart_items.find_by(product_id: product_id) # Busca um item existente no carrinho com o mesmo produto
+
+    # Se o item existe
+    if item
+      item.quantity += quantity  # Soma a quantidade enviada à quantidade atual
+      item.save                  # Salva no BD
+    # Senão
+    else
+      item = cart_items.create(product_id: product_id, quantity: quantity) # cria um novo item no carrinho com a quantidade informada
+    end
+
+    update_total_price # Chama metodo para calcular e atualizar o preço total do carrinho
+    item # Retorna o item
+  end
+
+  # Calcula e atualiza o preco total do carrinho
+  def update_total_price
+    new_total = cart_items.includes(:product).reduce(0) do |sum, item| # Cria um novo total, pegando todos os itens do produto, acumula a partir do 0
+      sum + (item.quantity * item.product.price) # Acumulador dos valores + (Qtd de itens * preco do item)
+    end
+
+    update_column(:total_price, new_total) # Atualiza a coluna do preco total no BD
+  end
+
+  # Verificação se Carrinho está abandonado
+  def abandoned?
+    updated_at < 3.hours.ago && status != 'abandoned' # Se não houver atualização há mais de 3 horas e não está marcado como abandonado
+  end
+
+  # Atualização de Status para Abandonado
+  def mark_abandoned
+    update(status: 'abandoned') # Atualiza o status de carrinho como abandonado
+  end
+
+  # Verificação se Carrinho deve ser deletado
+  def should_be_deleted?
+    status == 'abandoned' && updated_at < 7.days.ago # Verifica se o carrinho está abandonado e se a ultima atualização está a mais de 7 dias
+  end
+
+  # Remove o carrinho do banco
+  def remove_if_old
+    destroy if should_be_deleted? # Destroi o carrinho se a verificação do should_be_deleted? retornar true
+  end
 end
